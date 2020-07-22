92657f2da9f13c012a7d487b5dee0e0f323b91bc
Danielle Nicholson <daniellenicholsonart@gmail.com>
2020-07-22 11:23:26 -0600

    added a new hobby

--- js/scripts.js
+++ js/scripts.js
@@ -46,14 +46,29 @@ class Person {
         this.hobbies = hobbies; // current person we are one
     }
     //we can add methods to a class, they are like "functions" but are run from objects instead
-sayHello()//method
-{//crEATe an element
-    const helloElement = document.createElement( "p");
-    //fill in the text of element (using template literal.)
-    helloElement.textContent = `Hello, my name is ${this.name}!`;;
-    // add the new element to the body of our webpage
-    document.body.appendChild( helloElement);
-}
+    sayHello()//method
+    {//crEATe an element
+        const helloElement = document.createElement("p");
+        //fill in the text of element (using template literal.)
+        helloElement.textContent = `Hello, my name is ${this.name}!`;;
+        // add the new element to the body of our webpage
+        document.body.appendChild(helloElement);
+    }
+    birthday()// increments age property by one, via our 'birthday' method
+    {
+        this.age = this.age + 1;// make sure to use this to refer to current object
+    }
+    addHobbie(hobby = "") // parameter with default value
+    {//check if hobby is filled in
+        if (hobby.length > 0) //greater than blank string
+        {// if its filled push it to array
+            this.hobbies.push( hobby ); //push new hobbie on listy
+        }// if its not filled say so in console
+        else {
+            console.log("sorry, empty hobby. Add one!");
+
+        }
+    }
 }
 
 // let's make some peeps
@@ -62,12 +77,16 @@ console.log(jim);
 jim.sayHello(); //is a set of instructions, call upon a method to execute
 
 //when we pass arguments, the default parameters are over written
-const Sarah = new Person("Sarah" ,45,["sleep", "drawing"]); //passing through arguments
+const Sarah = new Person("Sarah", 45, ["sleep", "drawing"]); //passing through arguments
 console.log(Sarah);
 Sarah.height = "7'"; //we /can /update and add new properties (but should we probably not; its only update the one individual)
 console.log(Sarah);
 Sarah.sayHello();
 
- 
+//running the bday method
+Sarah.birthday();
+console.log("sarah is now " + Sarah.age + " years old")
 // Values kept inside an object are considered "properties."
 // Sets of instructions kept inside an object are considered "methods." 
+Sarah.addHobbie("yoga");
+console.log( Sarah.hobbies );
